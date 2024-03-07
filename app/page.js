e554ff014ba07e0a63ae9045678be988a38a6c9e
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-03-07 18:50:19 +0530

    Finish static recipe blog project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
+import getPostMetadata from "@/utils/getPostMetadata";
+import PostCard from "@/components/PostCard";
 
 
 export default function Home() {
+
+  const postMetadata = getPostMetadata('recipes')
+  console.log(postMetadata)
   return (
-   <main></main>
+    <main>
+      <div className="postsContainer">
+        {postMetadata.map((post, postIndex) => {
+          return (
+            <PostCard key={postIndex} post={post} />
+          )
+        })}
+      </div>
+    </main>
   );
 }
